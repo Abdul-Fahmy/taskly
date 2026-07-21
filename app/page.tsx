98d0efa8f942994c1633225dbd82a7d21cb30374
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2026-07-21 20:35:46 +0300

    fix: remove the hash code from hom page and pputt it in a component that wrap the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,10 @@
-"use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  const router = useRouter();
+export default async function HomePage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access_token")?.value;
 
-  useEffect(() => {
-    const hash = window.location.hash.substring(1);
-    if (!hash) return
-    if (hash) {
-      const params = new URLSearchParams(hash);
-      if (params.get("type") === "recovery" && params.get("access_token")) {
-        router.replace(`/reset-password?${hash}`);
-        return;
-      }
-    }
-
-    fetch("/api/user")
-      .then((res) => {
-        router.replace(res.ok ? "/projects" : "/login");
-      })
-      .catch(() => {
-        router.replace("/login");
-      });
-  }, [router]);
-
-  return null;
+  redirect(token ? "/project" : "/login");
 }
