98a9109db486db286444dabfa2033033b0312662
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2026-07-29 20:45:45 +0300

    implement members for project

---
+++ app/api/project/[projectId]/members/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { getMembers } from "@/app/services/member.service";
+
+export async function GET(req:Request,{ params }: {params: Promise<{ projectId: string }> }) {
+    const { projectId} =  await params;
+  const token = (await cookies()).get("access_token")?.value;
+
+  if (!token) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    const projects = await getMembers(projectId);
+    return NextResponse.json(projects);
+  } catch {
+    return NextResponse.json(
+      { message: "Failed to fetch projects" },
+      { status: 401 },
+    );
+  }
+}
